f6ebc49c8852257b4ba35030c8dac0e6285a3f4f
lovememo <lovememo@foxmail.com>
2017-08-25 13:35:36 +0800

    comments

--- chapter8/main.py
+++ chapter8/main.py
@@ -1,23 +1,23 @@
+# -*- coding: utf-8 -*-
 import regression
 import matplotlib.pyplot as plt
 
 from numpy import *
 xArr, yArr = regression.loadDataSet('ex0.txt')
-print 'ok'
-print xArr[0:2]
 ws = regression.standRegres(xArr, yArr)
-print ws
+
 
 xMat=mat(xArr)
 yMat=mat(yArr)
-
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
-
+ax = fig.add_subplot(111) #将画布分成1行1列，将从左到右，从上到下第一块显示图画
+ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0],c='purple',label='realData',marker='.')
+#scatter散点图
+# matrix[a:b,c:d]  第a到b行，且第c到d列 左闭右开
+# matrix[a,b] 第a行，第b列
 xCopy=xMat.copy()
-xCopy.sort(0)
+xCopy.sort(0)#y轴方向排序
 yHat=xCopy*ws
-ax.plot(xCopy[:,1],yHat)
+ax.plot(xCopy[:,1],yHat,c='green')
 
 plt.show()
