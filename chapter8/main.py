57821d3f60dede023716e167717a3e6fcd08b74a
lovememo <lovememo@foxmail.com>
2017-08-25 10:41:32 +0800

    main.py

---
+++ chapter8/main.py
@@ -0,0 +1,23 @@
+import regression
+import matplotlib.pyplot as plt
+
+from numpy import *
+xArr, yArr = regression.loadDataSet('ex0.txt')
+print 'ok'
+print xArr[0:2]
+ws = regression.standRegres(xArr, yArr)
+print ws
+
+xMat=mat(xArr)
+yMat=mat(yArr)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
+
+xCopy=xMat.copy()
+xCopy.sort(0)
+yHat=xCopy*ws
+ax.plot(xCopy[:,1],yHat)
+
+plt.show()
--- chapter8/regression.py
+++ chapter8/regression.py
@@ -1,27 +1,29 @@
+# -*- coding: utf-8 -*-
 from numpy import *
 
+def standRegres(xArr,yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T #行向量转列向量
+    xTx = xMat.T*xMat
+    if linalg.det(xTx) == 0.0: #计算行列式
+        print "This matrix is singular, cannot do inverse" #奇异矩阵不可逆
+        return
+    ws = xTx.I * (xMat.T*yMat)
+    return ws
 
-def loadDataSet(fileName):
-    numFeat = len(open(fileName).readline().split('\t')) - 1
-    dataMat = []
-    labelMat = []
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    numFeat = len(open(fileName).readline().split('\t')) - 1 #get number of fields 
+    dataMat = []; labelMat = []
     fr = open(fileName)
     for line in fr.readlines():
-        lineArr = []
-        curLine = line.strip().split('\t')
-    for i in range(numFeat):
-        lineArr.append(float(curLine[i]))
-    dataMat.append(lineArr)
-    labelMat.append(float(curLine[-1]))
-    return dataMat, labelMat
+        lineArr =[]
+        curLine = line.strip().split('\t') #strip -> trim
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1])) # -1 -> last element
+    return dataMat,labelMat
 
 
-def standRegres(xArr, yArr):
-    xMat = mat(xArr)
-    yMat = mat(yArr).T
-    xTx = xMat.T * xMat
-    if linalg.det(xTx) == 0.0:
-        print "This matrix is singular, cannot do inverse"
-    return
-    ws = xTx.I * (xMat.T * yMat)
-    return ws
+
+
