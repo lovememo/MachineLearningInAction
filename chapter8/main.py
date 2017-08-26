2afa216c6138e75fdec421454175d5e188895676
lovememo <lovememo@foxmail.com>
2017-08-26 17:22:08 +0800

    gaussweight

--- chapter8/main.py
+++ chapter8/main.py
@@ -1,23 +1,48 @@
 # -*- coding: utf-8 -*-
-import regression
+import regression as re
 import matplotlib.pyplot as plt
 
 from numpy import *
-xArr, yArr = regression.loadDataSet('ex0.txt')
-ws = regression.standRegres(xArr, yArr)
+
+def testStandRegres():
+    xArr, yArr = re.loadDataSet('ex0.txt')
+    ws = re.standRegres(xArr, yArr)
+    xMat=mat(xArr)
+    yMat=mat(yArr)
+    fig = plt.figure()
+    ax = fig.add_subplot(111) #将画布分成1行1列，将从左到右，从上到下第一块显示图画
+    ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0],c='purple',label='realData',marker='.')
+    #scatter散点图
+    # matrix[a:b,c:d]  第a到b行，且第c到d列 左闭右开
+    # matrix[a,b] 第a行，第b列
+    xCopy=xMat.copy()
+    #xCopy.sort(0)#y轴方向排序
+    yHat=xCopy*ws
+    ax.plot(xCopy[:,1],yHat,c='green')
+    # print yHat.T.flatten().A[0].size
+    # print yMat.flatten().A[0].size
+    # print yHat
+    # print yMat
+    correlation = corrcoef(yHat.T, yMat )
+    print correlation
+    plt.show()
+
+def testGaussWeight():
+    xArr, yArr = re.loadDataSet('ex0.txt')
+    xMat = mat(xArr)
+    #xArr.sort(0)
+    xMat.sort(0)
+    weights = re.getGaussWeightArr([1,0.5], xMat, 0.4)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(xMat[:,1], weights)    
+    plt.show()
+def main():
+    #testStandRegres()
+    testGaussWeight()
+    print 'end'
+main();
 
 
-xMat=mat(xArr)
-yMat=mat(yArr)
-fig = plt.figure()
-ax = fig.add_subplot(111) #将画布分成1行1列，将从左到右，从上到下第一块显示图画
-ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0],c='purple',label='realData',marker='.')
-#scatter散点图
-# matrix[a:b,c:d]  第a到b行，且第c到d列 左闭右开
-# matrix[a,b] 第a行，第b列
-xCopy=xMat.copy()
-xCopy.sort(0)#y轴方向排序
-yHat=xCopy*ws
-ax.plot(xCopy[:,1],yHat,c='green')
 
-plt.show()
+
--- chapter8/regression.py
+++ chapter8/regression.py
@@ -11,6 +11,47 @@ def standRegres(xArr,yArr):
     ws = xTx.I * (xMat.T*yMat)
     return ws
 
+#Locally Weighted Linear Regression LWLR （局部加权线性回归）
+#返回局部回归系数.
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    weights = getGaussWeightMat(testPoint, xArr, k)
+    xTx = xMat.T * (weights * xMat)#根据矩阵乘法结合律，这里没有括号也是可以的，加括号易于理解
+    if linalg.det(xTx) == 0:
+        print "This matrix is singular, cannot do inverse" #奇异矩阵不可逆
+        return
+    ws = xTx.I * xMat.T * (weights * yMat)
+    return ws
+
+def getGaussWeightMat(fixedPoint, inputPoints, k=1):
+    m = shape(inputPoints)[0]#对样本数据的每一个元素进行加权，所以权重对角矩阵的长度肯定是样本数据个数
+    gaussWeightMat = mat(eye(m))#权重对角矩阵 参看numpy.eye
+    for i in range(m):
+        diffMat =  fixedPoint - inputPoints[i,:]
+        gaussWeightMat[i,i] = exp(diffMat * diffMat.T / (-2 * k ** 2))
+    return gaussWeightMat
+
+def getGaussWeightArr(fixedPoint, inputPoints, k=1):
+    gaussWeightMat = getGaussWeightMat(fixedPoint, inputPoints, k)
+    m = shape(gaussWeightMat)[0]
+    gaussWeightArr = []
+    for i in range(m):
+        gaussWeightArr.append(gaussWeightMat[i,i])
+    return gaussWeightArr
+    
+    
+
+#testArr为行向量数组
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        ws = lwlr(testArr[i], xArr, yArr, k)
+        yHat[i] = testArr[i] * ws
+    return yHat
+        
+
 def loadDataSet(fileName):      #general function to parse tab -delimited floats
     numFeat = len(open(fileName).readline().split('\t')) - 1 #get number of fields 
     dataMat = []; labelMat = []
